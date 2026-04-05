0938c31e4ca46e7fd8081a32aca3371a272da860
Mrbysco <Mrbysco@users.noreply.github.com>
2026-04-06 00:26:18 +0200

    Remove debug log

--- common/src/main/java/com/mrbysco/armorposer/client/gui/SavePoseScreen.java
+++ common/src/main/java/com/mrbysco/armorposer/client/gui/SavePoseScreen.java
@@ -32,12 +32,10 @@ public class SavePoseScreen extends Screen {
 
 	protected void updateState() {
 		Vec3 currentOffset = this.parentScreen.getOffset();
-		Reference.LOGGER.info("Offset {}", currentOffset);
 		this.hasOffset = currentOffset != null && (currentOffset.x() != 0.0 || currentOffset.y() != 0.0 || currentOffset.z() != 0.0);
 
 		ListTag actions = this.parentScreen.getRecordedActions();
 		this.hasActions = !(actions == null || actions.isEmpty());
-		Reference.LOGGER.info("Actions {}", hasActions);
 	}
 
 	@Override
